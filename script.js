6ca482a8edabb529174db446e56c9a794bd88e84
kauasilvasodre <kauasodre730@gmail.com>
2024-04-26 09:52:09 -0500

    fist commit

---
+++ script.js
@@ -0,0 +1,24 @@
+<script>
+
+
+let dia = prompt("digite seu numero da sorte")
+
+if ( dia == 1){
+    document.write("Domingo dia 'de la pelota' ");
+}else if ( dia == 2){
+    document.write("segunda, dia de atividads em casa")
+}else if ( dia == 3){
+    document.write("terça, dia de peito e tricipes")
+}else if ( dia == 4){
+    document.write("quarta, dia de ler um bom maga")
+}else if ( dia == 5){
+    document.write("quinta, dia de tomar bom refrigente")
+}else if ( dia == 6){
+    document.write("sexta dia de assistir meus anime")
+}else if ( dia == 7){
+    document.write("sabado: piscininha e churrasco")
+}else {
+    document.write("nao indetificada")
+}
+
+</script>
